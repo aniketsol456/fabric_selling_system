ed3026abe176a610d6ffe6e76c3fee97a5fe0610
Aniket Pareshbhai Solanki <92724192+aniketsol456@users.noreply.github.com>
2024-10-31 20:54:54 +0530

    Change in app.js and adding some images

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Navbar from './Components/Navbar';
 import {BrowserRouter,Routes,Route} from 'react-router-dom';
 import Login from './Components/Login';
 import Register from './Components/Register';
+import Home from './Components/Home';
+import Footer from './Components/Footer';
 
 const App = () => {
   return (
@@ -11,9 +13,11 @@ const App = () => {
       <div className='Container'>
         <Navbar/>
         <Routes>
+          <Route exact path='/' element = {<Home/>}/>
           <Route exact path='/login' element = {<Login/>}/>
           <Route exact path='/register' element = {<Register/>}/>
         </Routes>
+        <Footer/>
       </div>
       </>
     </BrowserRouter>
