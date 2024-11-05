a1c8e2dfef26c6cfef1971a80f3559fe8c17eacd
Aniket Pareshbhai Solanki <92724192+aniketsol456@users.noreply.github.com>
2024-11-05 19:56:59 +0530

    Creating a Profile page

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -1,14 +1,48 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import User from '../assets/Images/user.jpg';
 import './Profile.css';
+import { useNavigate } from 'react-router-dom';
+import { LoginContext } from './ContextProvider/Context';
 
 const Profile = () => {
+
+    const  {logindata,setLogindata} = useContext(LoginContext);
+    // console.log(logindata.ValidUserOne.email);
+
+    const history = useNavigate();
+
+    const profilevalid = async () => {
+        let token = localStorage.getItem("usersdatatoken");
+
+        const res = await fetch("/validuser", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": token
+            }
+        });
+
+        const data = await res.json();
+        if (data.status == 401 || !data) {
+            history("*");
+        }
+        else {
+            console.log("user verify");
+            setLogindata(data);
+            history("/profile");
+        }
+    }
+
+    useEffect(() => {
+        profilevalid();
+    }, [])
+
     return (
         <>
             <div className='Info'>
-                <img src= {User} alt=''/>
-                <h1>Name : Aniket</h1><br/><br/>
-                <h1>User Email :    Aniket@gmail.com</h1>
+                <img src={User} alt='' />
+                <h1>Name :  {logindata ? logindata.ValidUserOne.fname: ""}</h1><br /><br />
+                <h1>User Email : {logindata ? logindata.ValidUserOne.email: ""}</h1>
             </div>
         </>
     )
