f23bc1c5e56e712a39337a893ff1c1a70ce9d8b0
Aniket Pareshbhai Solanki <92724192+aniketsol456@users.noreply.github.com>
2024-11-05 19:54:07 +0530

    Creating a error.js page

--- src/Components/Error.js
+++ src/Components/Error.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import errorphoto from '../assets/Images/404.svg';
 
 const Error = () => {
   return (
     <>
       <div className="container">
         <div style={{ minHeight: "85vh", display: "flex", justifyContent: "center", flexDirection: "column", alignItems: "center" }}>
-          <img src="/404.svg" alt="error" style={{ width: "500px", marginBottom: 20 }} />
+          <img src={errorphoto} alt="error" style={{ width: "500px", marginBottom: 20 }} />
           {/* <h1 className="mb-3">404 ERROR </h1> */}
           <h2 className="mb-3">PAGE NOT FOUND</h2>
           <NavLink to="/" className="btn btn-primary" style={{ fontSize: 18 }}> Back To Home Page </NavLink>
