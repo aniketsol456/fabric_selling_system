f8a31cfbf48e93bdd7e022dcf59168e442ebc465
Aniket Pareshbhai Solanki <92724192+aniketsol456@users.noreply.github.com>
2024-11-12 21:33:29 +0530

    add a path of cart page

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import FabricCatalog from './Components/Fabric_catalog';
 import Feedback from './Components/Feedback';
 import Help_Center from './Components/Help_Center';
 import Order from './Components/Order';
+import Cart from './Components/Cart';
 
 const App = () => {
   return (
@@ -24,6 +25,7 @@ const App = () => {
           <Route exact path='/fabric' element = {<FabricCatalog/>}/>
           <Route exact path='/feedback' element = {<Feedback/>}/>
           <Route exact path='/order' element = {<Order/>}/>
+          <Route exact path='/cart' element = {<Cart/>}/>
           <Route exact path='/helpcenter' element = {<Help_Center/>}/>
           <Route exact path='/register' element = {<Register/>}/>
           <Route exact path='/profile' element = {<Profile/>}/>
